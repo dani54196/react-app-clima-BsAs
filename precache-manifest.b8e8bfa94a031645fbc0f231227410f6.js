1e092e2c1b4a0226e5dc151579178262c504b87a
Daniel <dani54196@gmail.com>
2020-09-16 00:09:59 -0300

    Updates

---
+++ precache-manifest.b8e8bfa94a031645fbc0f231227410f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132ff463c7ff04d078d222b3da51d079",
+    "url": "/react-app-clima-BsAs/index.html"
+  },
+  {
+    "revision": "e962efaf72a3b203c012",
+    "url": "/react-app-clima-BsAs/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9fd4bc14a9042de3ebaa",
+    "url": "/react-app-clima-BsAs/static/css/main.d2b879cb.chunk.css"
+  },
+  {
+    "revision": "e962efaf72a3b203c012",
+    "url": "/react-app-clima-BsAs/static/js/2.32725310.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-clima-BsAs/static/js/2.32725310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fd4bc14a9042de3ebaa",
+    "url": "/react-app-clima-BsAs/static/js/main.2d457598.chunk.js"
+  },
+  {
+    "revision": "a5bd4e78ce27b185cc0e",
+    "url": "/react-app-clima-BsAs/static/js/runtime-main.53b4e624.js"
+  }
+]);
